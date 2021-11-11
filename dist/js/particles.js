5ac7b148aa340ed0c38f41a80ed84c986a680e69
RDOToole89 <roibinotoole@gmail.com>
2021-11-11 15:50:23 +0100

    add particles.js for hero background

---
+++ dist/js/particles.js
@@ -0,0 +1,8 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+window.particlesJS.load(
+  'particles-js',
+  '../../dist/js/particles.json',
+  function () {
+    console.log('callback - particles.js config loaded');
+  }
+);
