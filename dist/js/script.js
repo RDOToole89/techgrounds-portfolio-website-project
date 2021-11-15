595043a625c9fbec944b6e7c02c2b7414b8aa038
RDOToole89 <roibinotoole@gmail.com>
2021-11-15 15:44:25 +0100

    finish first v1 of header => mobile first design

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,12 +1,15 @@
 const hamburgerMenu = document.querySelector('.hamburger');
-const hiddenMenu = document.querySelector('.hamburger-nav-list');
-
+const navigation = document.querySelector('.nav');
+const listItems = document.querySelectorAll('.nav__list-item');
 
 hamburgerMenu.addEventListener('click', () => {
-  
-  console.log('hamburger button clicked!')
-  
+  console.log('hamburger button clicked!');
+
   hamburgerMenu.classList.toggle('open');
-  hiddenMenu.classList.toggle('show-hidden-nav');
+  navigation.classList.toggle('nav-open');
+  navigation.classList.toggle('open');
+
+  [...listItems].forEach((item) => item.classList.toggle('open-item'));
 });
 
+console.log(listItems);
