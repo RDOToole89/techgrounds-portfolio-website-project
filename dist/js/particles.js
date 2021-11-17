901d4ee3c631a20cd3af9ad31795232f675910ac
RDOToole89 <roibinotoole@gmail.com>
2021-11-17 17:02:16 +0100

    add hover effect to picture / end of day commit

--- dist/js/particles.js
+++ dist/js/particles.js
@@ -1,8 +1,38 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
-window.particlesJS.load(
-  'particles-js',
-  '../../dist/js/particles.json',
-  function () {
-    console.log('callback - particles.js config loaded');
+
+const windowNameArray = window.location.pathname.split('/');
+const windowName = windowNameArray[windowNameArray.length - 1];
+
+const selectParticle = (windowName) => {
+  switch (windowName) {
+    case 'contact.html':
+      window.particlesJS.load(
+        'particles-js',
+        '../../dist/js/particles.json',
+        function () {
+          console.log('callback - particles.js config loaded');
+        }
+      );
+      break;
+    case 'index.html': {
+      window.particlesJS.load(
+        'particles-js',
+        '../../dist/js/particles.json',
+        function () {
+          console.log('callback - particles.js config loaded');
+        }
+      );
+      break;
+    }
+    default:
+      break;
   }
-);
+};
+
+ window.particlesJS.load(
+        'particles-js',
+        '../../dist/js/particles.json',
+        function () {
+          console.log('callback - particles.js config loaded');
+        }
+      );
